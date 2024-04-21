8c27294673cf2d3c1ca145399fe8143714028d60
Kamit6337 <kamit6337@gmail.com>
2024-04-21 21:55:00 +0530

    update

--- middlewares/globalMiddlwares.js
+++ middlewares/globalMiddlwares.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import passport from "passport";
 import { corsOptions } from "../utils/corsOptions.js";
 import session from "express-session";
 import expressSessionOptions from "../utils/expressSessionOptions.js";
@@ -12,25 +11,10 @@ const globalMiddlewares = (app) => {
 
   app.use(cors(corsOptions));
 
-  // app.set("view engine", "ejs");
-  // Serve static files from the public folder
-  //   app.use(express.static(path.join(__dirname, "public")));
-  app.use(express.static("public"));
-
-  // Middleware to create a time session of cookie (login, jwt)
-  // app.use(session(expressSessionOptions));
-  // app.use(cookieSession(cookieSessionOptions));
-
   app.use(session(expressSessionOptions));
-
-  // Middleware to parse cookies
-  // app.use(cookieParser());
 
   // Middleware to parse incoming body
   app.use(bodyParser.json());
-
-  app.use(passport.initialize());
-  app.use(passport.session());
 
   // Middleware to parse JSON request bodies
   app.use(express.json());
