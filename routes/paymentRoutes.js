7d746a192fa69cb0c4d4dcf357be288bf217caad
Kamit6337 <kamit6337@gmail.com>
2024-05-27 16:55:08 +0530

    done

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,6 @@
 import express from "express";
 import makePaymentSession from "../controllers/payment/makePaymentSession.js";
 import afterSuccessfulPayment from "../controllers/payment/afterSuccessfulPayment.js";
-import afterFailedPayment from "../controllers/payment/afterFailedPayment.js";
-import paymentMiddleware from "../middlewares/paymentMiddleware.js";
 
 const router = express.Router();
 
